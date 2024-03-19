f7e96a060d80e9f8dc251f31e554d009271d7685
Ahmed Saber <a.saber.abdelmohsen@gmail.com>
2024-03-19 22:12:22 +0200

    add openAPI specs to backend

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,9 +1,14 @@
 import express, {Request, Response} from 'express'
 import routes from './routes';
 import helmet from 'helmet';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs'; // For YAML files
 var cors = require('cors')
 
 const app = express();
+
+const openapiDocument = YAML.load('./openapi.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiDocument));
 
 app.use(cors())
 app.use(express.json());
